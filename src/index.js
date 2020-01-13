5568f3262a53eba9575aa9048fed5c7972bdff4e
steventt07 <steventt07@tamu.edu>
2020-01-12 23:29:55 -0600

    Added more pages and linked them together

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
-import React, { useLayoutEffect } from "react"
-import ReactDOM from "react-dom"
+import React, { useLayoutEffect } from "react";
+import ReactDOM from "react-dom";
 
-import { 
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Link,
-    Redirect} from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 //Pages
-import MainPage from "./pages/index"
-import AboutPage from "./pages/about"
-import NotFoundPage from "./pages/404"
+import MainPage from "./pages/index";
+import AboutPage from "./pages/about";
+import FeaturePage from "./pages/feature";
+import TraderPage from "./pages/trader";
+import NotFoundPage from "./pages/404";
 
 ReactDOM.render(
-    <Router>
-        <Switch>
-            <Route exact path="/" component={MainPage}/>
-            <Route exact path="/about" component={AboutPage}/>
-            <Route exact path="/404" component={NotFoundPage}/>
-            <Redirect to="/404"/>
-        </Switch>
-    </Router>, 
-    document.getElementById("root"))
+  <Router>
+    <Switch>
+      <Route exact path="/" component={MainPage} />
+      <Route exact path="/about" component={AboutPage} />
+      <Route exact path="/feature" component={FeaturePage} />
+      <Route exact path="/trader" component={TraderPage} />
+      <Route exact path="/404" component={NotFoundPage} />
+      <Redirect to="/404" />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
