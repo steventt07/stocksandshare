abe2d4f6ea9727f15c4f2f2739b6a5d545baff6a
steventt07 <steventt07@tamu.edu>
2020-01-20 11:43:34 -0600

    Updated backlog and fixed headers overflowing

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,8 +1,28 @@
 import React from "../../../node_modules/react";
 import "../../index.css";
 import { Link } from "../../../node_modules/react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 function Header() {
+  const classes = useStyles();
   let styles = {
     "text-decoration": "none",
     color: "#FFFFFF",
@@ -13,29 +33,32 @@ function Header() {
     "text-decoration": "none",
     color: "#FFFFFF",
     padding: "0",
-    "padding-right": "20%",
     "margin-left": "10px"
 
     // "margin-left": "10px"
   };
   return (
-    <header className="navbar">
-      <Link style={home_styles} to="/">
-        Stocks and Share
-      </Link>
-      <Link style={styles} to="/about">
-        About
-      </Link>
-      <Link style={styles} to="/feature">
-        Feature
-      </Link>
-      <Link style={styles} to="/trader">
-        Trader
-      </Link>
-      <Link style={styles} to="/learn">
-        Learn
-      </Link>
-    </header>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+          <Link style={home_styles} to="/">
+            Stocks and Share
+          </Link>
+        </Typography>
+        <Link style={styles} to="/about">
+          About
+        </Link>
+        <Link style={styles} to="/feature">
+          Feature
+        </Link>
+        <Link style={styles} to="/trader">
+          Trader
+        </Link>
+        <Link style={styles} to="/learn">
+          Learn
+        </Link>
+      </Toolbar>
+    </AppBar>
   );
 }
 
