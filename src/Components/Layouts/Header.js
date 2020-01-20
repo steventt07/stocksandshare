5d85f9eb9b08828a632192a79b94b8580ae1eb5f
steventt07 <steventt07@tamu.edu>
2020-01-20 12:21:29 -0600

    updated header layout

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -5,9 +5,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,38 +26,67 @@ function Header() {
   const classes = useStyles();
   let styles = {
     "text-decoration": "none",
-    color: "#FFFFFF",
-    padding: "5px"
-    // "margin-left": "10px"
+    color: "#000000"
   };
   let home_styles = {
     "text-decoration": "none",
     color: "#FFFFFF",
     padding: "0",
     "margin-left": "10px"
+  };
 
-    // "margin-left": "10px"
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
   };
   return (
     <AppBar position="static">
       <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+          onClick={handleClick}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem>
+            <Link style={styles} to="/about">
+              About
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link style={styles} to="/feature">
+              Feature
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link style={styles} to="/trader">
+              Trader
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link style={styles} to="/learn">
+              Learn
+            </Link>
+          </MenuItem>
+        </Menu>
         <Typography variant="h6" className={classes.title}>
           <Link style={home_styles} to="/">
             Stocks and Share
           </Link>
         </Typography>
-        <Link style={styles} to="/about">
-          About
-        </Link>
-        <Link style={styles} to="/feature">
-          Feature
-        </Link>
-        <Link style={styles} to="/trader">
-          Trader
-        </Link>
-        <Link style={styles} to="/learn">
-          Learn
-        </Link>
       </Toolbar>
     </AppBar>
   );
